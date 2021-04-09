cd56c995bba4a4446d435b366cedbdfe7d925701
Nabeel Najjar <n.najjar@gmail.com>
2021-04-10 01:31:23 +0300

    1st package

---
+++ nnPackage/text.py
@@ -0,0 +1,8 @@
+def joke2():
+    ''' python-packaging '''
+    return (u'Wenn ist das Nunst\u00fcck git und Slotermeyer? Ja! ... '
+            u'Beiherhund das Oder die Flipperwaldt gersput.')
+# setup  => pip install -U .
+# usage  => nnPackage.joke()
+# Vact84dgTwfHg2R
+# distibution => python setup.py sdist
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='nnPackage',
+      version='0.1',
+      description='Nabeel Najjar Package 1',
+      url='http://github.com/storborg/funniest',
+      author='Nabeel Najjar',
+      author_email='N.Najjar@gmail.com',
+      license='MIT',
+      packages=['nnPackage'],
+      zip_safe=False)
